4c5c512b372ded355beb7a75d56505104f42f942
jerzybrzezinski <shogunpol@googlemail.com>
2026-06-22 18:54:58 +0100

    Added Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path')
 
+const postcssPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
 module.exports = {
     entry: './app/assets/scripts/App.js',
     output: {
@@ -7,5 +13,17 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     mode: 'development',
-    watch: true
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader", 
+                    { loader: "css-loader", options: { url: false } }, 
+                    { loader: "postcss-loader", options: { postcssOptions: { plugins: postcssPlugins } } } // Fixed the 'c' here!
+                ]
+            }
+        ]
+    }
 }
