65dfbf5e9189068d0ed9e0fc1e008587d0f84245
jerzybrzezinski <shogunpol@googlemail.com>
2026-06-29 11:15:43 +0100

    Introduce mixins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+
 const postcssPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
